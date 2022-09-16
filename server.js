016173ae40488e556ce7ca06e5816bd873370928
Khushwant <Technological.mess@gmail.com>
2022-09-16 14:57:08 -0400

    Added self-declation in server.js

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+/*********************************************************************************
+* WEB422 – Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+* 
+* Name: _KHUSHWANT SINGH RAO_ Student ID: __145931192___ Date: ___2022-09-16___
+* Cyclic Link: https://teal-nice-bee.cyclic.app/
+********************************************************************************/ 
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
